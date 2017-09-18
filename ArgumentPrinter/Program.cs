e2e7975ab59b5a639a62e078162fd2d95f3529db
Miguel Silva <miguelinho_28@hotmail.com>
2017-09-18 17:56:28 -0600

    First commit

---
+++ ArgumentPrinter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArgumentPrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(string arg in args)
+                Console.Write(arg + " ");
+            Console.ReadLine();
+        }
+    }
+}
